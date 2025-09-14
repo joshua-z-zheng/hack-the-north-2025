2c5ace372d6abd3cc521169868d69b53687cfe53
Davis Liu <davisliu2006@gmail.com>
2025-09-14 01:04:11 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,30 @@
-export interface Market {
-  threshold: number
-  probability: number
-  shares?: number
+export class Market {
+  threshold: number = 0;
+  probability: number = 0;
+  shares?: number;
 }
-export interface Course {
-  code: string
-  odds: Market[]
+
+export class Course {
+  code: string = "";
+  odds: Market[] = [];
+
+  constructor(code: string) {
+    this.code = code;
+    this.odds = [
+      {
+        threshold: 85,
+        probability: 0
+      },
+      {
+        threshold: 90,
+        probability: 0
+      }
+    ]
+  }
+}
+
+export class User {
+  email: string = "";
+  sub: string = "";
+  courses: Course[] = [];
 }
